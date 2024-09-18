75c352d5117949dd36a575a38fd87718c4f24b6f
fishert3844 <fishert3844@student.faytechcc.edu>
2024-09-18 10:00:06 -0400

    Add files via upload

---
+++ P1HW1_Tim_Fisher.py
@@ -0,0 +1,56 @@
+# Tim Fisher
+# 09/16/2024
+# P1HW1
+# Create a program that uses mathematical functions
+
+
+# Explaing what
+
+print("-----Calculating Exponents-----")
+
+# Get input from user for base value
+
+base_value = input("Enter an interger as the base value: ")
+
+# Convert base value to base value as integer
+
+base_value = int(base_value)
+
+# Get input from user and convert to integer
+
+exponent = int(input("Enter integer as the exponent "))
+
+print(base_value, "raised to the power of", exponent, "is", base_value**exponent, "!!")
+
+
+# -----Addition and Subtraction-----
+
+
+# Get integer input from user
+
+# Starting integer
+
+start_int = input('Enter a starting integer:')
+start_int = int(start_int)
+
+# Add an integer
+
+add_int = input('Enter an integer to add:')
+add_int = int(add_int)
+
+# Add starting integer and addition integer
+
+add_total = start_int + add_int
+add_total - int(add_total)
+
+
+# Subtract an integer
+
+sub_int = input('Enter an integer to subtract:')
+sub_int = int(sub_int)
+
+# Total of all three integers
+
+total_int = add_total - sub_int
+                       
+print(start_int, '+', add_int, '-', sub_int, 'is equal to', total_int )
